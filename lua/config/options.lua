e7b77e55be9c0abc455e749bf906646e7eb7ec3e
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-04-14 15:31:46 -0400

    UPDATE config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,11 +3,6 @@
 -- Add any additional options here
 
 local set = vim.opt -- Generals options (Windows/Files)
-local cmd = vim.cmd -- for VimScript commands
 
 set.colorcolumn = "80"
-set.history = 1000 -- Sets how many lines of history VIM has to remember
-set.undofile = true -- Save undos after file closes
-set.undolevels = 1000 -- How many undos
-set.undoreload = 10000 -- Number of lines to save for undo
-cmd("set undodir=~/.config/nvim/undodir") -- where to save undo histories, lua have a trouble with path HOME path '~''
+set.wrap = true -- line wrap
