15d8b8261414093ab9dc177426f45f8b0fc26ac0
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-04-19 22:38:44 -0400

    UPDATE cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,10 +2,21 @@ return {
   "hrsh7th/nvim-cmp", -- Ensure this is the correct plugin
   event = "InsertEnter", -- You can adjust this based on your LazyVim setup
   config = function()
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
     local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
     -- Define the configuration directly in cmp.setup
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
       -- Completion sources
       sources = {
         { name = "copilot", group_index = 2 },
@@ -17,12 +28,34 @@ return {
       },
 
       -- Key mappings for completion
-      mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping.select_next_item(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-      }),
+      mapping = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            -- cmp.select_next_item()
+            cmp.confirm({ select = true })
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      },
 
       -- Enable experimental features like ghost text
       experimental = {
