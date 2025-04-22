8568f4a6e5771a854bf091af01b6ca28efd2258b
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-04-22 10:21:40 -0400

    UPDATE Visual Multi

--- lua/plugins/install-defaults.lua
+++ lua/plugins/install-defaults.lua
@@ -3,7 +3,6 @@ return {
   "turbio/bracey.vim",
   "rhysd/git-messenger.vim",
   "tpope/vim-commentary",
-  "mg979/vim-visual-multi",
   "MattesGroeger/vim-bookmarks",
   "kylechui/nvim-surround",
   "nvim-treesitter/nvim-treesitter",
