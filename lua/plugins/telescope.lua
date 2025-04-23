ed1de22c4504aaf0625b3c19edb6ace9e2ec4d6a
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-04-23 09:01:57 -0400

    UPDATE

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,12 +2,15 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = {
     defaults = {
-      file_ignore_patterns = { "node_modules", ".git" },
-      hidden = true, -- ← Esto muestra archivos ocultos
+      hidden = true,
+      file_ignore_patterns = {
+        "node_modules",
+        "^%.git/", -- this Pattern is used to ignore the .git directory and not the .gitignore
+      },
     },
     pickers = {
       find_files = {
-        hidden = true, -- ← También aplica a esta picker
+        hidden = true,
       },
     },
   },
