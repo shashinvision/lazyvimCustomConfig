4502b4104e49df33a178f9600f4e61d77b8ebf49
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-04-26 21:38:24 -0400

    Update to angular and csharp setting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,16 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        omnisharp = {}, -- OmniSharp activo
-        csharp_ls = { enabled = false }, -- <<-- Desactiva csharp_ls
-      },
-    },
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers.omnisharp = {
+        cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+        enable_roslyn_analyzers = true,
+        organize_imports_on_format = true,
+        enable_import_completion = true,
+      }
+      opts.servers.tsserver = opts.servers.tsserver or {}
+      opts.servers.eslint = opts.servers.eslint or {}
+    end,
   },
 }
