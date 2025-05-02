795fe95e3e860c506e475dd017cb43e828501ab6
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-02 00:24:22 -0400

    update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,33 @@
--- return {
---   {
---     "neovim/nvim-lspconfig",
---     opts = {
---       servers = {
---         omnisharp = {}, -- OmniSharp activo
---         csharp_ls = { enabled = false }, -- <<-- Desactiva csharp_ls
---       },
---     },
---   },
--- }
 return {
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
       opts.servers = opts.servers or {}
+
       opts.servers.omnisharp = {
         cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
         enable_roslyn_analyzers = true,
         organize_imports_on_format = true,
         enable_import_completion = true,
       }
+
       opts.servers.tsserver = opts.servers.tsserver or {}
       opts.servers.eslint = opts.servers.eslint or {}
+
+      opts.servers.emmet_ls = {
+        filetypes = {
+          "html",
+          "css",
+          "javascript",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+          "vue",
+          "blade",
+          "php",
+          "xml",
+        },
+      }
     end,
   },
 }
