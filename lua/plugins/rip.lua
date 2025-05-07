1ef7710da68ad4dce3ab24bccb3b5dea48560d5b
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-07 18:30:18 -0400

    RIP replace

--- lua/plugins/rip.lua
+++ lua/plugins/rip.lua
@@ -1,15 +1,11 @@
 return {
-  -- Plugin: nvim-rip-substitute
-  -- URL: https://github.com/chrisgrieser/nvim-rip-substitute
-  -- Description: A Neovim plugin for performing substitutions with ripgrep.
   "chrisgrieser/nvim-rip-substitute",
 
   cmd = "RipSubstitute", -- Command to trigger the plugin
 
   keys = {
     {
-      -- Keybinding to perform a ripgrep substitution
-      "<leader>fs",
+      "<leader>R",
       function()
         require("rip-substitute").sub() -- Call the substitution function from the plugin
       end,
