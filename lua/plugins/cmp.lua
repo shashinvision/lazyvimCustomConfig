62f8b59ebed92b2c0ea67d2634cbe6299fc06d9e
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-08 14:11:44 -0400

    update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -32,7 +32,6 @@ return {
     },
     config = function(_, opts)
       local cmp = require("cmp")
-      local luasnip = require("luasnip")
 
       -- Configuración base
       opts = vim.tbl_deep_extend("force", {
@@ -53,24 +52,6 @@ return {
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
         },
         formatting = {
           format = require("lspkind").cmp_format({
