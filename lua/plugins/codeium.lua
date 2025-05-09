6737bfc742e332d5db23fbf8c6fe11b234deb3aa
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-09 08:20:17 -0400

    Update avante

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -4,9 +4,46 @@ return {
   -- "Exafunction/windsurf.vim", -- Replace with the actual plugin name
 
   event = "VeryLazy",
+
   keys = {
-    { "<leader>ac", "<cmd>Codeium Chat<CR>", desc = "Open Codeium Chat" },
-    { "<leader>aA", "<cmd>Codeium Auth<CR>", desc = "Authenticate Codeium" },
+    { "<leader>ac", "<cmd>Codeium Chat<CR>", desc = "Codeium Chat" },
+    { "<leader>aA", "<cmd>Codeium Auth<CR>", desc = "Codeium Auth" },
+    {
+      "<C-g>",
+      mode = "i",
+      function()
+        return vim.fn["codeium#Accept"]()
+      end,
+      expr = true,
+      desc = "Codeium Accept",
+    },
+    {
+      "<C-x>",
+      mode = "i",
+      function()
+        return vim.fn["codeium#Clear"]()
+      end,
+      expr = true,
+      desc = "Codeium Clear",
+    },
+    {
+      "<C-]>",
+      mode = "i",
+      function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end,
+      expr = true,
+      desc = "Codeium Next",
+    },
+    {
+      "<C-[>",
+      mode = "i",
+      function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end,
+      expr = true,
+      desc = "Codeium Prev",
+    },
   },
   config = function()
     require("codeium").setup({
