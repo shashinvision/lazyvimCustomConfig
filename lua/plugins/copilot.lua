28bcfe40b998bc85f4e3f56a1334402ae49ee61d
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-11 19:52:20 -0400

    Update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,15 +2,15 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   build = ":Copilot auth",
-  dependencies = {
-    "zbirenbaum/copilot-cmp",
-  },
+  -- dependencies = {
+  --   "zbirenbaum/copilot-cmp",
+  -- },
   opts = {
     suggestion = { enabled = false },
     panel = { enabled = false },
   },
   config = function(_, opts)
     require("copilot").setup(opts)
-    require("copilot_cmp").setup()
+    -- require("copilot_cmp").setup()
   end,
 }
