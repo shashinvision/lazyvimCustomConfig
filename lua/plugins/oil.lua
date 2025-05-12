9a86a07a89bbe52e06ff07a199fbe447f603ad7b
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-11 20:30:55 -0400

    update oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,5 +1,11 @@
 return {
   "stevearc/oil.nvim",
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+  },
   keys = {
     {
       "-",
