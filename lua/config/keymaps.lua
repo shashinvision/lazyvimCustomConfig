532b65c2fbb53708aa83ee8a218d111c56e0722e
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-12 11:28:25 -0400

    ADD keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,7 +12,7 @@ keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Clean :%s/^M//g
-keymap.set("n", "<leader>cM", ":s/^M//g<CR>", { noremap = true, silent = true, desc = "Borrar los espacios en blanco" })
+keymap.set("n", "<leader>cM", ":s/^M//g<CR>", { noremap = true, silent = true, desc = "Delete blank spaces(^M)" })
 
 keymap.set("n", "<leader>[", "<S-$>%", { noremap = true, desc = "Move to end {([])}" }) -- Te lleva al final o el principio de llave relacionada {}[]()
 
