f770abc565a3d1fd231d8501d1d995f6de28fef5
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-13 23:29:20 -0400

    Update conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,31 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        html = { "prettier" },
-        typescript = { "prettier" },
-        cs = { "csharpier" },
-      },
-      formatters = {
-        prettier = {
-          command = "prettier",
-          args = { "--stdin-filepath", "$FILENAME" },
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      -- Web formats
+      html = { "prettier" },
+      vue = { "prettier" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      -- Angular support
+      ["*.component.html"] = { "prettier" },
+      -- C#
+      cs = { "csharpier" },
+      -- Optional: CSS/SCSS if needed
+      css = { "prettier" },
+      scss = { "prettier" },
+    },
+    formatters = {
+      prettier = {
+        prepend_args = {
+          "--parser",
+          "html", -- For HTML/Angular
+          "--parser",
+          "vue", -- For Vue files
+          "--parser",
+          "typescript", -- For TS files
         },
       },
     },
