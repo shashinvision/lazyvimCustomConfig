13470b296877a392a82296860281b0a788be9158
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-05-23 08:48:20 -0400

    lsp mason update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,41 @@
 return {
   {
-    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
     opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "selene",
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "tailwindcss-language-server",
+        "typescript-language-server", -- corregido
+        "css-lsp",
+        "omnisharp",
+        "eslint-lsp",
+        "html-lsp",
+        "svelte-language-server",
+        "angular-language-server",
+        "graphql-language-service-cli",
+        "emmet-ls", -- este es válido para mason, pero usaremos emmet_ls en lspconfig
+        "prisma-language-server",
+        "pyright",
+        "ruff",
+        "json-lsp",
+        "yaml-language-server",
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "folke/neodev.nvim",
+    },
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.inlay_hints = { enabled = true }
       opts.servers = opts.servers or {}
 
       opts.servers.omnisharp = {
@@ -11,27 +45,70 @@ return {
         enable_import_completion = true,
       }
 
-      opts.servers.tsserver = opts.servers.tsserver or {}
-      opts.servers.eslint = opts.servers.eslint or {}
-
-      opts.ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "angularls",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-        "ruff_lsp",
-        "eslint",
-        "tailwindcss",
-        "jsonls",
-        "omnisharp",
+      opts.servers.tailwindcss = {
+        root_dir = function(...)
+          return require("lspconfig.util").root_pattern(".git")(...)
+        end,
       }
+
+      opts.servers.tsserver = {
+        root_dir = function(...)
+          return require("lspconfig.util").root_pattern(".git")(...)
+        end,
+        single_file_support = false,
+        settings = {
+          completions = {
+            completeFunctionCalls = true,
+          },
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "literal",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = false,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+            target = "ESNext",
+            module = "ESNext",
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+            target = "ESNext",
+            module = "ESNext",
+          },
+        },
+      }
+
+      opts.servers.eslint = {
+        settings = {
+          packageManager = "npm",
+          codeActionOnSave = {
+            enable = true,
+            mode = "all",
+          },
+        },
+        on_attach = function(client, bufnr)
+          if client.name == "eslint" then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                vim.cmd("EslintFixAll")
+              end,
+            })
+          end
+        end,
+      }
+
       opts.servers.emmet_ls = {
         filetypes = {
           "html",
@@ -51,42 +128,99 @@ return {
         init_options = {
           html = {
             options = {
-              -- Para forzar atributos con comillas
               ["bem.enabled"] = true,
             },
           },
         },
       }
-      opts.servers.tsserver = {
+
+      opts.servers.yamlls = {
         settings = {
-          completions = {
-            completeFunctionCalls = true,
-          },
-          javascript = {
-            target = "ESNext",
-            module = "ESNext",
-          },
-          typescript = {
-            target = "ESNext",
-            module = "ESNext",
+          yaml = {
+            keyOrdering = false,
           },
         },
       }
-      opts.servers.eslint = {
+
+      opts.servers.lua_ls = {
+        single_file_support = true,
         settings = {
-          packageManager = "npm", -- o "yarn", "pnpm"
-          codeActionOnSave = {
-            enable = true,
-            mode = "all",
+          Lua = {
+            workspace = { checkThirdParty = false },
+            completion = {
+              workspaceWord = true,
+              callSnippet = "Both",
+            },
+            hint = {
+              enable = true,
+              setType = false,
+              paramType = true,
+              paramName = "Disable",
+              semicolon = "Disable",
+              arrayIndex = "Disable",
+            },
+            doc = { privateName = { "^_" } },
+            type = { castNumberToInteger = true },
+            diagnostics = {
+              disable = { "incomplete-signature-doc", "trailing-space" },
+              groupSeverity = {
+                strong = "Warning",
+                strict = "Warning",
+              },
+              groupFileStatus = {
+                ["ambiguity"] = "Opened",
+                ["await"] = "Opened",
+                ["codestyle"] = "None",
+                ["duplicate"] = "Opened",
+                ["global"] = "Opened",
+                ["luadoc"] = "Opened",
+                ["redefined"] = "Opened",
+                ["strict"] = "Opened",
+                ["strong"] = "Opened",
+                ["type-check"] = "Opened",
+                ["unbalanced"] = "Opened",
+                ["unused"] = "Opened",
+              },
+              unusedLocalExclude = { "_*" },
+            },
+            format = {
+              enable = false,
+              defaultConfig = {
+                indent_style = "space",
+                indent_size = "2",
+                continuation_indent_size = "2",
+              },
+            },
           },
         },
-        on_attach = function(_, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-        end,
       }
+
+      opts.servers.cssls = {}
+      opts.servers.html = {}
+
+      return opts
+    end,
+
+    config = function(_, opts)
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      -- Viene en la configuracion por defecto y esta deprecado
+      -- De esta manera lo desactivamos
+      opts.servers.ruff_lsp = nil
+
+      for server, server_opts in pairs(opts.servers or {}) do
+        local config_module = lspconfig[server]
+        if config_module and type(config_module.setup) == "function" then
+          local final_opts = vim.tbl_deep_extend("force", {
+            capabilities = capabilities,
+          }, server_opts or {})
+
+          config_module.setup(final_opts)
+        else
+          vim.notify("[LSP] El servidor '" .. server .. "' no está disponible en lspconfig.", vim.log.levels.WARN)
+        end
+      end
     end,
   },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,6 +20,7 @@ return {
       "yaml",
       "php",
       "css",
+      "scss",
       "prisma",
       "svelte",
       "graphql",
@@ -30,6 +31,29 @@ return {
       "elixir",
       "c",
       -- Add more as needed
+    },
+    query_linter = {
+      enable = true,
+      use_virtual_text = true,
+      lint_events = { "BufWrite", "CursorHold" },
+    },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = true, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = "o",
+        toggle_hl_groups = "i",
+        toggle_injected_languages = "t",
+        toggle_anonymous_nodes = "a",
+        toggle_language_display = "I",
+        focus_language = "f",
+        unfocus_language = "F",
+        update = "R",
+        goto_node = "<cr>",
+        show_help = "?",
+      },
     },
     highlight = {
       enable = true,
@@ -48,4 +72,16 @@ return {
       },
     },
   },
+
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+
+    -- MDX
+    vim.filetype.add({
+      extension = {
+        mdx = "mdx",
+      },
+    })
+    vim.treesitter.language.register("markdown", "mdx")
+  end,
 }
