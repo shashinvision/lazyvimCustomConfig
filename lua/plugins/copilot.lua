133bc0b439adfa04293becc44fa5a608d4752941
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-06-06 13:39:55 -0400

    fix IA

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -10,12 +10,6 @@ return {
         suggestion = {
           enabled = false,
           auto_trigger = true,
-          keymap = {
-            accept = "<C-CR>", -- acepta la sugerencia con Tab
-            next = "<C-n>", -- siguiente sugerencia
-            prev = "<C-p>", -- anterior sugerencia
-            dismiss = "<C-e>", -- descartar sugerencia
-          },
         },
         panel = { enabled = false },
       })
