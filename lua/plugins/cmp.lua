71808095a0d071ccc341b1126bbb3e8d38c1f224
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-06-17 10:46:23 -0400

    update snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,31 +17,36 @@ return {
       "Exafunction/windsurf.nvim",
 
       -- Snippets
-      { "saadparwaiz1/cmp_luasnip" },
-      { "rafamadriz/friendly-snippets" },
-      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
-      {
-        "honza/vim-snippets",
-        lazy = true,
-      },
       {
         "L3MON4D3/LuaSnip",
-        opts = {
-          config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-            require("luasnip.loaders.from_snipmate").lazy_load({
-              paths = {
-                vim.fn.stdpath("data") .. "/lazy/vim-snippets",
-                vim.fn.stdpath("config") .. "/snippets",
-              },
-            })
-
-            -- Extender filetypes
-            local ls = require("luasnip")
-            ls.filetype_extend("typescript", { "javascript" })
-            ls.filetype_extend("typescriptreact", { "javascript", "html" })
-          end,
+        version = "v2.*",
+        build = "make install_jsregexp",
+        dependencies = {
+          "rafamadriz/friendly-snippets",
+          "saadparwaiz1/cmp_luasnip",
+          { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+          {
+            "honza/vim-snippets",
+            lazy = true,
+          },
         },
+        lazy = true,
+        config = function()
+          local ls = require("luasnip")
+
+          -- Extensiones de filetype
+          ls.filetype_extend("typescript", { "javascript" })
+          ls.filetype_extend("typescriptreact", { "javascript", "html" })
+
+          -- Carga de snippets
+          require("luasnip.loaders.from_vscode").lazy_load()
+          require("luasnip.loaders.from_snipmate").lazy_load({
+            paths = {
+              vim.fn.stdpath("data") .. "/lazy/vim-snippets",
+              vim.fn.stdpath("config") .. "/snippets",
+            },
+          })
+        end,
       },
 
       -- Emmet integration
