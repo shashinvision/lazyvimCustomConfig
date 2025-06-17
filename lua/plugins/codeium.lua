97cefcb19a7bc898b56f6a2000bf6ffd992ae229
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-06-17 12:47:39 -0400

    update snippets

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -3,7 +3,7 @@ return {
     "Exafunction/windsurf.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "saghen/blink.cmp",
+      "hrsh7th/nvim-cmp",
     },
 
     keys = {
--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -135,23 +135,4 @@ return {
       chat.setup(opts)
     end,
   },
-  -- Blink integration
-  {
-    "saghen/blink.cmp",
-    optional = true,
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      sources = {
-        providers = {
-          path = {
-            -- Path sources triggered by "/" interfere with CopilotChat commands
-            enabled = function()
-              return vim.bo.filetype ~= "copilot-chat"
-            end,
-          },
-        },
-      },
-    },
-  },
 }
