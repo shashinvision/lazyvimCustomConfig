0b2760547649e2f5ee8abaa72e49c6b8ccf998c7
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-06-17 09:54:38 -0400

    update snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -11,7 +11,10 @@ return {
         require("luasnip.loaders.from_vscode").lazy_load()
 
         require("luasnip.loaders.from_snipmate").lazy_load({
-          paths = vim.fn.stdpath("data") .. "/lazy/vim-snippets",
+          paths = {
+            vim.fn.stdpath("data") .. "/lazy/vim-snippets",
+            vim.fn.stdpath("config") .. "/snippets",
+          },
         })
       end,
     },
