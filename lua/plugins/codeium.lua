22c954ecce79d25aab376e9730cf9b6b680df1be
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-06-21 21:46:49 -0400

    update

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -7,9 +7,49 @@ return {
     },
 
     keys = {
+      -- Atajos en modo inserción (estilo Copilot)
+      {
+        "<C-CR>",
+        function()
+          return vim.fn["codeium#Accept"]()
+        end,
+        mode = "i",
+        expr = true,
+        desc = "Codeium: Accept",
+      },
+      {
+        "<C-p>",
+        function()
+          return vim.fn["codeium#CycleCompletions"](1)
+        end,
+        mode = "i",
+        expr = true,
+        desc = "Codeium: Next",
+      },
+      {
+        "<C-n>",
+        function()
+          return vim.fn["codeium#CycleCompletions"](-1)
+        end,
+        mode = "i",
+        expr = true,
+        desc = "Codeium: Prev",
+      },
+      {
+        "<C-e>",
+        function()
+          return vim.fn["codeium#Clear"]()
+        end,
+        mode = "i",
+        expr = true,
+        desc = "Codeium: Clear",
+      },
+
+      -- Comandos en modo normal
       { "<leader>aA", "<cmd>Codeium Auth<cr>", mode = "n", desc = "Codeium: Auth" },
       { "<leader>aC", "<cmd>Codeium Chat<cr>", mode = "n", desc = "Codeium Chat" },
     },
+
     config = function()
       require("codeium").setup({
         enable_chat = true,
